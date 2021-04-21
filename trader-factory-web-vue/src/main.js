8dd0ddb2b74cd39b0a93cb871d4e6a0bf6ff707f
nunofontes <nunof.h.fontes@gmail.com>
2021-04-21 22:06:03 +0100

    configuring fake backend

--- trader-factory-web-vue/src/main.js
+++ trader-factory-web-vue/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// setup fake backend
+import { configureFakeBackend } from './_helpers';
+configureFakeBackend();
+
 createApp(App).mount("#app");
