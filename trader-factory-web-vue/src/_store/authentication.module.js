73ff2b3a4754fb934b3d4495a484174d9597a8a9
nunofontes <nunof.h.fontes@gmail.com>
2021-04-23 22:40:57 +0100

    added authentication store module with state and some mutations

---
+++ trader-factory-web-vue/src/_store/authentication.module.js
@@ -0,0 +1,37 @@
+
+const user = JSON.parse(sessionStorage.getItem('user'));
+const initialState = user
+    ? { status: { loggedIn: true }, user }
+    : { status: {}, user: null };
+
+export const authentication = {
+    namespaced: true,
+    state: initialState,
+    actions: {
+        login() {
+
+        },
+        logout() {
+
+        }
+    },
+    mutations: {
+        loginRequest(state, user) {
+            state.status = { loggingIn: true };
+            state.user = user;
+        },
+        loginSuccess(state, user) {
+            state.status = { loggedIn: true };
+            state.user = user;
+        },
+        loginFailure(state) {
+            state.status = {};
+            state.user = null;
+        },
+        logout(state) {
+            state.status = {};
+            state.user = null;
+        }
+    }
+
+}
