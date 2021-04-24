a458c5516fba1d06f4e253f2947ef25d2cf23d82
nunofontes <nunof.h.fontes@gmail.com>
2021-04-24 21:49:35 +0100

    added Navbar on root page

--- trader-factory-web-vue/src/App.vue
+++ trader-factory-web-vue/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Navbar />
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Navbar
   },
 };
 </script>
