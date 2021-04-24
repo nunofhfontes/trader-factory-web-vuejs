d1322100a745bb42ead17bced0040b234199db30
nunofontes <nunof.h.fontes@gmail.com>
2021-04-24 21:49:47 +0100

    configured router

--- trader-factory-web-vue/src/main.js
+++ trader-factory-web-vue/src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from './_helpers/router';
 
 // setup fake backend
 import { configureFakeBackend } from './_helpers';
 configureFakeBackend();
 
-createApp(App).mount("#app");
+// createApp(App).mount("#app");
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
