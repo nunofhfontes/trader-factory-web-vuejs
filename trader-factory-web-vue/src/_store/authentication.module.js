3a17a2024a0450d911c2c2dda011421121d4ab06
nunofontes <nunof.h.fontes@gmail.com>
2021-05-05 19:53:45 +0100

    auth module update

--- trader-factory-web-vue/src/_store/authentication.module.js
+++ trader-factory-web-vue/src/_store/authentication.module.js
@@ -1,37 +1,31 @@
-
-const user = JSON.parse(sessionStorage.getItem('user'));
+const user = JSON.parse(sessionStorage.getItem("user"));
 const initialState = user
-    ? { status: { loggedIn: true }, user }
-    : { status: {}, user: null };
+  ? { status: { loggedIn: true }, user }
+  : { status: {}, user: null };
 
 export const authentication = {
-    namespaced: true,
-    state: initialState,
-    actions: {
-        login() {
-
-        },
-        logout() {
-
-        }
+  namespaced: true,
+  state: initialState,
+  actions: {
+    login() {},
+    logout() {},
+  },
+  mutations: {
+    loginRequest(state, user) {
+      state.status = { loggingIn: true };
+      state.user = user;
     },
-    mutations: {
-        loginRequest(state, user) {
-            state.status = { loggingIn: true };
-            state.user = user;
-        },
-        loginSuccess(state, user) {
-            state.status = { loggedIn: true };
-            state.user = user;
-        },
-        loginFailure(state) {
-            state.status = {};
-            state.user = null;
-        },
-        logout(state) {
-            state.status = {};
-            state.user = null;
-        }
-    }
-
-}
+    loginSuccess(state, user) {
+      state.status = { loggedIn: true };
+      state.user = user;
+    },
+    loginFailure(state) {
+      state.status = {};
+      state.user = null;
+    },
+    logout(state) {
+      state.status = {};
+      state.user = null;
+    },
+  },
+};
